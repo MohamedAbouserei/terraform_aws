1c6ed8199b5823de2f568c9e70caf660ffc60888
Mohamed Abouserei <49098554+MohamedAbouserei@users.noreply.github.com>
2020-04-05 18:18:56 +0200

    Update configuration.tf

--- configuration.tf
+++ configuration.tf
@@ -1,13 +1,15 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
-    
+  shared_credentials_file = "credentials"
+  profile = "access"
 }
 terraform {
   backend "s3" {
+    shared_credentials_file = "credentials"
+    profile = "access"
     bucket = "terraform-iti"
     key    = "state/state.tfstate"
     region = "us-east-1"
-     
   }
 }
