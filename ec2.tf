5ef0a3f04eae555d821f5c444a26e60e94c3ff79
Mohamed Abouserei <49098554+MohamedAbouserei@users.noreply.github.com>
2020-04-05 19:27:42 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec1" {
-  ami           = "ami-0fc61db8544a617ed"
+  ami           = "ami-0e01ce4ee18447327"
   instance_type = "t2.micro"
   subnet_id     = "${aws_subnet.prod-subnet-public-1.id}"
     key_name ="${aws_key_pair.generated_key.id}"
@@ -7,7 +7,7 @@ resource "aws_instance" "ec1" {
 
 }
 resource "aws_instance" "ec2" {
-  ami           = "ami-0fc61db8544a617ed"
+  ami           = "ami-0e01ce4ee18447327"
   instance_type = "t2.micro"
   subnet_id     = "${aws_subnet.prod-subnet-public-1.id}"
     key_name ="${aws_key_pair.generated_key.id}"
@@ -15,20 +15,20 @@ resource "aws_instance" "ec2" {
 
 }
 resource "aws_instance" "ec3" {
-  ami           = "ami-0fc61db8544a617ed"
+  ami           = "ami-0e01ce4ee18447327"
   instance_type = "t2.micro"
   subnet_id     = "${aws_subnet.prod-subnet-public-2.id}"
   key_name ="${aws_key_pair.generated_key.id}"
 }
 resource "aws_instance" "ec4" {
-  ami           = "ami-0fc61db8544a617ed"
+  ami           = "ami-0e01ce4ee18447327"
   instance_type = "t2.micro"
   subnet_id     = "${aws_subnet.prod-subnet-private-1.id}"
     key_name ="${aws_key_pair.generated_key.id}"
 
 }
 resource "aws_instance" "ec5" {
-  ami           = "ami-0fc61db8544a617ed"
+  ami           = "ami-0e01ce4ee18447327"
   instance_type = "t2.micro"
   subnet_id     = "${aws_subnet.prod-subnet-private-2.id}"
   key_name ="${aws_key_pair.generated_key.id}"
