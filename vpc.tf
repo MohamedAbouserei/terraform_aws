5eac90c2a5f0f5caf6d116efd253f2f76b39df09
Mohamed Abouserei <49098554+MohamedAbouserei@users.noreply.github.com>
2020-04-05 19:04:34 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "prod-subnet-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-2a"
     tags {
         Name = "prod-subnet-public-1"
     }
@@ -19,7 +19,7 @@ resource "aws_subnet" "prod-subnet-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true" //it makes this a public subnet
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-2a"
     tags {
         Name = "prod-subnet-public-2"
     }
@@ -27,7 +27,7 @@ resource "aws_subnet" "prod-subnet-public-2" {
 resource "aws_subnet" "prod-subnet-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.3.0/24"
-    availability_zone = "us-east-1c"
+    availability_zone = "us-east-2c"
     tags {
         Name = "prod-subnet-private-1"
     }
@@ -35,7 +35,7 @@ resource "aws_subnet" "prod-subnet-private-1" {
 resource "aws_subnet" "prod-subnet-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
-    availability_zone = "us-east-1c"
+    availability_zone = "us-east-2c"
     tags {
         Name = "prod-subnet-private-2"
     }
